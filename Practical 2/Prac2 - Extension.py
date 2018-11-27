1cb131b04d2fc036ded34ab402d7c6ae05e08a51
nhantrithong <trithong.nhan@my.jcu.edu.au>
2018-11-27 16:26:29 +0800

    finalized scratch exercise #1

--- Practical 2/Prac2 - Extension.py
+++ Practical 2/Prac2 - Extension.py
@@ -1,8 +1,11 @@
 def main():
-    character = input("Input a character: ")
-    print("The ASCII code for",character,"is",ord(character))
+    try:
+        character = input("Input a character: ")
+        print("The ASCII code for",character,"is",ord(character))
+    except TypeError:
+        print("Invalid input")
     number = int(input("Please enter a number between {} and {}".format(33,127)))
-    while int(number) < 33 or int(number) > 127:
+    while number < 33 or number > 127:
         number = input("Please re-input a number between 33 and 127")
     else:
         print("The character for",number,"is",chr(number))
--- Practical 4/Prac4 - Scratch1.py
+++ Practical 4/Prac4 - Scratch1.py
@@ -1,22 +1,22 @@
+import random
+
+NUM_PER_LINES = 6
+MIN = 1
+MAX = 45
+
 def main():
-    list = []
-    import random
-    pick_num = int(input("How many quick picks would you like to generate?"))
-    for i in range(pick_num):
-        print("")
-        for randnum in range(6):
-            x=random.randint(1,46)
-            if x not in list:
-                list.append(x)
-            else:
-                randnum-=1
-            list.sort()
-        for num in list:
-            print (num, end=' ')
-        list=[]
-
-
-
-
+    num_picks = int(input("Enter number to quick picks to be generated"))
+    while num_picks < 0:
+        print("Please re-enter a valid value")
+        num_picks = int(input("Enter number to quick picks to be generated"))
+    for i in range(num_picks):
+        quick_picks = []
+        for j in range(NUM_PER_LINES):
+            number = random.randint(MIN,MAX)
+            while number in quick_picks:
+                nummber = random.randint(MIN,MAX)
+            quick_picks.append(number)
+        quick_picks.sort()
+        print(" ".join("{:4}".format(number) for number in quick_picks))
 
 main()
