e2c52aab97917a0ad0e7e03de8ff62166fefebb4
nhantrithong <trithong.nhan@my.jcu.edu.au>
2018-12-04 10:02:02 +0800

    added and finished intermediate exer 1

--- Practical 5/Prac 5 - Exer1.py
+++ Practical 5/Prac 5 - Exer1.py
@@ -7,15 +7,15 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+print(STATE_NAMES)
 
-#state = input("Enter short state: ").upper()
-#while state != "":
-    #if state in STATE_NAMES:
-        #print(state, "is", STATE_NAMES[state])
-    #else:
-        #print("Invalid short state")
-    #state = input("Enter short state: ")
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in STATE_NAMES:
+        print(state, "is", STATE_NAMES[state])
+    else:
+        print("Invalid short state")
+    state = input("Enter short state: ")
 
-for key,value in STATE_NAMES.items():
-    print("{:<3} is {}".format(key,value))
+#for key,value in STATE_NAMES.items():
+    #print("{:<3} is {}".format(key,value))
