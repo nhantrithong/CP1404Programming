8b61d2ef7d576fcfe79fab0d90198e6ceedb7e72
nhantrithong <trithong.nhan@my.jcu.edu.au>
2018-12-04 11:00:32 +0800

    testing func to count words

---
+++ Practical 5/Prac 5 - Scartch1.py
@@ -0,0 +1,16 @@
+#def main():
+    #counts = dict()
+    #text = input("Enter string:")
+    #divide = text.split()
+    #for words in divide:
+        #counts[words] = counts.get(words,0) + 1
+        #print("{}:{}".format(words, counts[words]))
+
+    #print(counts)
+
+def main():
+    list = []
+    text = input("Enter string:")
+    list.append(text)
+    print(list)
+main()
