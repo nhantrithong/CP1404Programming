7efdf7911016e4f9e68315c65ff8c2f4e687bdf6
nhantrithong <trithong.nhan@my.jcu.edu.au>
2018-12-04 09:31:01 +0800

    added prac5 folder

---
+++ Powerpoint Activities/Ppt8.py
@@ -0,0 +1,34 @@
+#Write a loop that prints Olympic years (every 4 years) from 1990 to now
+#def main():
+    #for i in range(1990,2019,4):
+        #print(i)
+
+#main()
+
+
+#Write a function that returns the average value of a list of numbers passed into it
+#import random
+#list = []
+#def main():
+    #for i in range(10):
+        #generated = random.randint(1,21)
+        #list.append(generated)
+    #average = sum(list) / len(list)
+    #print("The average number from the list is",average)
+
+#main()
+
+
+#Write a function that returns the median value (middle when sorted) of a list of numbers passed in to it
+#import random
+#import statistics
+#list = []
+#def main():
+    #for i in range(10):
+        #generated = random.randint(1,21)
+        #list.append(generated)
+    #list.sort()
+    #print(list)
+    #print("The median of the list is",statistics.median(list))
+
+#main()
