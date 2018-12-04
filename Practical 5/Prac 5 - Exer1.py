037c2b0f39108da66621653f9617249c43573639
nhantrithong <trithong.nhan@my.jcu.edu.au>
2018-12-04 09:46:14 +0800

    added formatting for states

--- Practical 5/Prac 5 - Exer1.py
+++ Practical 5/Prac 5 - Exer1.py
@@ -9,10 +9,13 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
-state = input("Enter short state: ")
-while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
-    else:
-        print("Invalid short state")
-    state = input("Enter short state: ")
+#state = input("Enter short state: ").upper()
+#while state != "":
+    #if state in STATE_NAMES:
+        #print(state, "is", STATE_NAMES[state])
+    #else:
+        #print("Invalid short state")
+    #state = input("Enter short state: ")
+
+for key,value in STATE_NAMES.items():
+    print("{:<3} is {}".format(key,value))
